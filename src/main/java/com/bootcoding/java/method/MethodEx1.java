508721caa205c81cfcdee211562863f418da7ffd
Shreya102002 <shreyaswamy10@gmail.com>
2023-03-31 19:46:20 +0530

    square of n is calculated

--- src/main/java/com/bootcoding/java/method/MethodEx1.java
+++ src/main/java/com/bootcoding/java/method/MethodEx1.java
@@ -4,5 +4,10 @@ public class MethodEx1 {
 
     public static void main(String[] args) {
         System.out.println("H...");
+        int n = 10;
+
+        int square = n * n;
+
+        System.out.println("Square of " + n + " is " + square);
     }
 }
