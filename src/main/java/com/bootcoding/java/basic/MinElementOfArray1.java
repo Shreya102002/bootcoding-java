daccee25b3e2833c08e379beac23bef64b0b90d4
Shreya102002 <shreyaswamy10@gmail.com>
2023-03-31 20:31:01 +0530

    min elements program

---
+++ src/main/java/com/bootcoding/java/basic/MinElementOfArray1.java
@@ -0,0 +1,14 @@
+package com.bootcoding.java.basic;
+
+class MinElementOfArray1 {
+    public static void main(String args[]) {
+        int a[] = {23, 33, 45, 51, 66, 78};
+        int min = a[0];
+        for (int k = 0; k < a.length; k++) {
+            if (a[k] < min) {
+                min = a[k];
+            }
+        }
+        System.out.println(min);
+    }
+}
