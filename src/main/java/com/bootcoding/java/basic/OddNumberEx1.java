99a600dbd870d73a0ea6f77eaa1c3a680d989a93
Shreya102002 <shreyaswamy10@gmail.com>
2023-03-31 20:31:38 +0530

    odd numbers program

---
+++ src/main/java/com/bootcoding/java/basic/OddNumberEx1.java
@@ -0,0 +1,16 @@
+package com.bootcoding.java.basic;
+
+public class OddNumberEx1 {
+    public static void main(String[] args) {
+
+        int num = 100;
+
+        if (num % 2 != 0) {
+            System.out.println("Number is odd");
+        }
+	else{
+                System.out.println("Number is not  odd");
+            }
+        }
+
+}
---
+++ src/main/java/com/bootcoding/java/basic/OddNumberExample.java
@@ -0,0 +1,15 @@
+package com.bootcoding.java.basic;
+
+public class OddNumberExample {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            } else {
+                System.out.println("");
+            }
+        }
+    }
+}
