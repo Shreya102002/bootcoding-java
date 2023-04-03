7ff2d0aed3790707044d6cdae038a768afd2e22f
Shreya102002 <shreyaswamy10@gmail.com>
2023-04-03 18:33:46 +0530

    while loop

---
+++ src/main/java/com/bootcoding/java/basic/SystemprintInjava.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java.basic;
+
+public class SystemprintInjava {
+    public static void main(String[] args) {
+        int num = 100;
+        System.out.println("list of odd numbers from 1 to" + num );
+        for (int p = 1; p <= num; p++) {
+            System.out.println(p);
+        }
+    }
+}
