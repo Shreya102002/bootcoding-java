d287d5c235e81c467687f1a8603a24d218653e22
Shreya102002 <shreyaswamy10@gmail.com>
2023-04-19 18:58:12 +0530

    printpaper

---
+++ src/main/java/com/bootcoding/java/Example/Printmsg.java
@@ -0,0 +1,10 @@
+package com.bootcoding.java.Example;
+
+import java.sql.SQLOutput;
+
+public class Printmsg {
+    public static void main (String args[]){
+        System.out.println("14\\04\\2023");
+
+    }
+}
---
+++ src/main/java/com/bootcoding/java/Example/Printpaper.java
@@ -0,0 +1,20 @@
+package com.bootcoding.java.Example;
+
+public class Printpaper {
+      String type;
+      String colour;
+      int size;
+      double price;
+      public static void main(String args[]){
+          Printpaper p1= new Printpaper();
+            p1.type="thin paper";
+         p1.colour="white";
+          p1.size=12;
+          p1.price=120;
+          System.out.println("p1.type =" + p1.type);
+        }
+
+
+
+    }
+
