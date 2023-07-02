c856b058e499b9debbc5024aabccb5afff5a8c87
Shreya102002 <shreyaswamy10@gmail.com>
2023-07-02 20:59:27 +0530

    addition two / three numbers

---
+++ src/main/java/com/bootcoding/java/Example/AdditionOfTwo.java
@@ -0,0 +1,11 @@
+package com.bootcoding.java.Example;
+
+public class AdditionOfTwo {
+    public static void main(String args[]){
+        int num1=24;
+        int num2=30;
+        int sum=num1+num2;
+          System.out.println(sum);
+    }
+
+}
---
+++ src/main/java/com/bootcoding/java/basic/AdditionOfThree.java
@@ -0,0 +1,15 @@
+package com.bootcoding.java.basic;
+
+import java.sql.SQLOutput;
+
+public class AdditionOfThree {
+    public static void main (String args[]){
+        System.out.println("The sum of three numbers");
+        int num1=3;
+        int num2=4;
+        int num3=5;
+        int sum =num1+num2+num3;
+        System.out.println(sum);
+    }
+
+}
