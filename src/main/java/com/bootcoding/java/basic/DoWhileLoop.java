6140c9c8ddaaea879878294982a24b053673f37b
Shreya102002 <shreyaswamy10@gmail.com>
2023-07-16 23:28:40 +0530

    while and do while loop

---
+++ src/main/java/com/bootcoding/java/basic/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package com.bootcoding.java.basic;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int a=0;
+        do{
+            System.out.println(a);
+            a++;
+        }
+        while(a<7);
+    }
+}
--- src/main/java/com/bootcoding/java/basic/WhileLoop.java
+++ src/main/java/com/bootcoding/java/basic/WhileLoop.java
@@ -2,11 +2,14 @@ package com.bootcoding.java.basic;
 
 public class WhileLoop {
     public static void main(String[] args) {
-        int i=1;
-        while(i<=3 ){
+        int i=100;
+        while(i<=200){
             System.out.println(i);
            i++;
-
+        }
+        System.out.println("finish running infinite loop");
+       // while(true){
+           // System.out.println("i m an infinite while loop");
         }
     }
-}
+
